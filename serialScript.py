d533829e6346048011087d941d7bd7cc0fed5de2
thinWatermelon <harry.x.ni@gmail.com>
2019-03-18 12:18:34 -0500

    Added correct Pi port

--- serialScript.py
+++ serialScript.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 import serial
 
-serialPort=serial.Serial(port='COM4', baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+serialPort=serial.Serial(port='/dev/ttyACM0', baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+
+
+
+
+
+
+seriali
+
+
+
 
