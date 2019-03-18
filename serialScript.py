2534e2742a6ba81987c28091ca7c94d5b481df86
Harry <harry.x.ni@gmail.com>
2019-03-18 12:39:20 -0500

    Text

--- serialScript.py
+++ serialScript.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import serial
 import io
+from pyfiglet import Figlet
+
+f=Figlet(font='slant')
 
 sr=serial.Serial(port='/dev/ttyACM0', baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-
-while (1=1):
+print(f.renderText('ArduSiPM'))
+while (1==1):
     print(sr.readline())
 
 
