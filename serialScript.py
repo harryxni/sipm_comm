8c9d522902506b029bc37f0a7c17cb0aff9f630b
thinWatermelon <harry.x.ni@gmail.com>
2019-03-19 12:01:27 -0500

    testing write command

--- serialScript.py
+++ serialScript.py
@@ -8,8 +8,9 @@ f=Figlet(font='slant')
 sr=serial.Serial(port='/dev/ttyACM0', baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 print(f.renderText('ArduSiPM'))
 x=1
+#sr.write(b'm')
 while (x==1):
-    print(sr.readline())
+    print(sr.readline().strip())
 
 
 
