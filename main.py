49930ab3770c446ae03f669a08751ddbca3af068
Harry <harry.x.ni@gmail.com>
2019-03-28 18:54:28 -0500

    Reorganized into two different python scripts, added some initial functions to work out

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import serial
+import io
+from pyfiglet import Figlet
+import sys
+f=Figlet(font='univers')
+print(f.renderText('ArduSiPM'))
+import serialScripts as ss
+
+#Main.py handles most of the command line input/output 
+
+
+on=True
+while (on):
+    print('Functionality: \n \"raw serial\" | Prints Raw Serial Output \n \"change HV\" [value (V)] | changes the High Voltage to value given\n \"count rate" [time (s)] | measure the count rate over given time in seconds' )
+    
+    mode = input('Mode: ')
+
+    if mode == ('raw serial'):
+        ss.rawSerial()
+    elif mode.contains('change HV'):
+        ss.changeHV()
+    elif mode.contains('count rate'):
+        ss.countRate()
